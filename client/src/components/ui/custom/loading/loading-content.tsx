1ae4c141067344c26629a9bd8f68a58c1ebb6797
minollisantiago <minollisantiago@gmail.com>
2025-04-16 14:27:37 -0300

    Fix: removed models and added the loading component on the custom ui components barrel

--- client/src/components/ui/custom/loading/loading-content.tsx
+++ client/src/components/ui/custom/loading/loading-content.tsx
@@ -9,6 +9,9 @@ interface Props {
   children: ReactNode;
 }
 
+// TODO: Create the custom errorComponent, you can use this as a starting example: 
+// https://github.com/elie222/inbox-zero/blob/379aead40c41827b6d7dea269e8c8300a0825090/apps/web/components/ErrorDisplay.tsx#L10
+
 export const LoadingContent = ({ loading, loadingComponent, error, errorComponent, children }: Props) => {
   return (
     <>
