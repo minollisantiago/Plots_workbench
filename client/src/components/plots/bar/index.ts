497ad4dd9525befe44cf542bce6c3290ed402759
minollisantiago <minollisantiago@gmail.com>
2025-05-20 09:13:19 -0300

    Docs: jsdoc description on the new plot component folders (index.ts) for histogram and bar plot components

--- client/src/components/plots/bar/index.ts
+++ client/src/components/plots/bar/index.ts
@@ -1,2 +1,8 @@
+/**
+ * This index.ts file serves as a central point for exporting components related to the bar plot.
+ * It exports the following:
+ *  - PlotBar: The main component that renders the bar plot with its figure.
+ *  - PlotBarFigure: The component responsible for rendering the actual bar plot figure using Plotly.js.
+ */
 export * from './plot-bar.tsx';
 export * from './plot-bar-figure.tsx';
--- client/src/components/plots/histogram/index.ts
+++ client/src/components/plots/histogram/index.ts
@@ -1,2 +1,8 @@
+/**
+ * This index.ts file serves as a central point for exporting components related to the histogram plot.
+ * It exports the following:
+ *  - PlotHist: The main component that renders the histogram plot with its figure.
+ *  - PlotHistFigure: The component responsible for rendering the actual histogram plot figure using Plotly.js.
+ */
 export * from './plot-hist.tsx';
 export * from './plot-hist-figure.tsx';
