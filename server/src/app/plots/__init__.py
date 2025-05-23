3579dd6201dee816bbaa5879fd0fe1a23849f35d
minollisantiago <minollisantiago@gmail.com>
2025-05-23 17:35:57 -0300

    Removed the python server from the repo

--- server/src/app/plots/__init__.py
+++
@@ -1,9 +0,0 @@
-# Figure Wrappers
-from .figure_style import FigureStyle
-from .figure_wrapper import FigureWrapper
-
-# Plotter Classes
-from .plotter_line import PlotterLine
-from .plotter_bar import PlotterBar
-from .plotter_scatter import PlotterScatter
-from .plotter_hist import PlotterHist
